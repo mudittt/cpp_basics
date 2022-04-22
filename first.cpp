44e370598fb80a03a51b8db80a70a3a457ecc2c4
mudit tyagi <mudittyagi2002@gmail.com>
2022-04-22 17:46:23 +0530

    about std namespace and iostream

---
+++ first.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    printf("hello");
+    std :: cout<<"Bye"; 
+    // ::    -> scope resolution
+    // cout  -> console out
+    
+    cout << "\nhahahaah"; 
+    //without writing -> (std::)
+    return 0;
+}
