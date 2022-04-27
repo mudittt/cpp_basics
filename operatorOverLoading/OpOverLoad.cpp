6b4f61e487d488ecc9c0facfad471ad4ac7305ab
mudit tyagi <mudittyagi2002@gmail.com>
2022-04-27 18:05:31 +0530

    operator using ''friend''

--- operatorOverLoading/OpOverLoad.cpp
+++ operatorOverLoading/OpOverLoad.cpp
@@ -29,13 +29,26 @@ public:
         temp.img = img + x.img;
         return temp;
     }
+
+    // we can also use a friend function to subtract two complex object
+    friend complex operator-(complex x, complex y);
 };
+
+// it is always recommended to define a ~ ~ ~friend operator overloading~ ~ ~ outside the class
+complex operator-(complex x, complex y)
+{
+    complex temp;
+    temp.real = x.real - y.real;
+    temp.img = x.img - y.img;
+    return temp;
+}
 
 int main(int argc, char const *argv[])
 {
     complex c1(5, 4);
     complex c2(7, 13);
     complex c3;
+    complex c4;
     /*c3 = c1.add(c2);*/
     cout << c3.real << endl;
     cout << c3.img << endl;
@@ -51,6 +64,10 @@ int main(int argc, char const *argv[])
     // compiler will look for 'operator+'
     cout << c3.real << endl;
     cout << c3.img << endl;
+
+    c4 = c1 - c2;
+    cout << c4.real << endl;
+    cout << c4.img << endl;
 
     return 0;
 }
